2f2d7495aa0c018f209030ef75d6a4c111df0994
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2026-06-04 23:33:13 +0700

    Add play_wav method to AudioEngine class
    Implemented play_wav method to play audio files and handle errors.

--- src/audio_tools/engine.py
+++ src/audio_tools/engine.py
@@ -1,9 +1,23 @@
+import sounddevice as sd
+import soundfile as sf
+
 class AudioEngine:
     def __init__(self):
         self.playing_objects = []
 
-    def play_wav(self, file_path):
-        pass
+    def play_wav(self, file_path: str):
+        try:
+            data, fs = sf.read(file_path)
+            sd.play(data, fs)
+            self._refresh_playing_objects()
+            self.playing_objects.append({"file": file_path, "status": "playing"})
+        except Exception as e:
+            print(f"\n[AudioEngine Error] failed to play! {file_path}: {e}")
 
     def wait_all(self):
-        pass
+        sd.wait()
+        self.playing_objects.clear()
+
+    def _refresh_playing_objects(self):
+        if not sd.get_stream().active:
+            self.playing_objects.clear()
